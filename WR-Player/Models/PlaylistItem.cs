3d6bb9e33efc8ed74aff74cdd060eeca5ba91d70
Elias Gasparis <iliasgas@gmail.com>
2019-01-14 15:11:07 +0200

    Moved some code from PlaylistItem class to the new PlaylistItemHelper class.

--- WR-Player/Models/PlaylistItem.cs
+++ WR-Player/Models/PlaylistItem.cs
@@ -44,8 +44,8 @@ namespace WR_Player.Models
             set
             {
                 _path = value;
-                generateType();
-                generateDuration();
+                Type = PlaylistItemHelper.GenerateType(Path);
+                DurationInSeconds = PlaylistItemHelper.GenerateDuration(Path, Type);
                 NotifyPropertyChanged();
             }
         }
@@ -80,69 +80,6 @@ namespace WR_Player.Models
             {
                 _isSelected = value;
                 NotifyPropertyChanged();
-            }
-        }
-
-        private void generateType()
-        {
-            if (Path.ToLower().StartsWith("http"))
-            {
-                Type = AudioType.Url;
-                return;
-            }
-            string extension = System.IO.Path.GetExtension(Path);
-            extension = extension.Substring(1); //first character is '.' so we remove it
-            extension = extension.First().ToString().ToUpper() + extension.Substring(1).ToLower();
-            Enum.TryParse(extension, out AudioType type);
-            Type = type;
-        }
-
-        private void generateDuration()
-        {
-            if (Type == AudioType.Url)
-            {
-                DurationInSeconds = -1;
-                return;
-            }
-
-            string durationString = getDurationStringFromShell();
-            DurationInSeconds = parseDurationStringToSeconds(durationString);
-        }
-
-        private string getDurationStringFromShell()
-        {
-            FileInfo fileInfo = new FileInfo(Path);
-            string directoryName = fileInfo.DirectoryName;
-            string filename = fileInfo.Name;
-
-            string fileLength = string.Empty;
-
-            var shell = new Shell();
-            var directory = shell.NameSpace(directoryName);
-            foreach (FolderItem2 file in directory.Items())
-            {
-                if (file.Name == filename)
-                    fileLength = directory.GetDetailsOf(file, 27).Trim();
-            }
-            Marshal.ReleaseComObject(directory);
-            Marshal.ReleaseComObject(shell);
-
-            return fileLength;
-        }
-
-        private int parseDurationStringToSeconds(string durationString)
-        {
-            try
-            {
-                int hours = Int32.Parse(durationString.Substring(0, 2));
-                int minutes = Int32.Parse(durationString.Substring(3, 2));
-                int seconds = Int32.Parse(durationString.Substring(6, 2));
-                TimeSpan tp = new TimeSpan(hours, minutes, seconds);
-                return (int)tp.TotalSeconds;
-            }
-            catch (Exception)
-            {
-                return 0;
             }
         }
 
---
+++ WR-Player/Models/PlaylistItemHelper.cs
@@ -0,0 +1,75 @@
+﻿using Shell32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WR_Player.Models
+{
+    static class PlaylistItemHelper
+    {
+        public static AudioType GenerateType(string path)
+        {
+            if (path.ToLower().StartsWith("http"))
+                return AudioType.Url;
+            string extension = System.IO.Path.GetExtension(path);
+            extension = extension.Substring(1); //first character is '.' so we remove it
+            extension = extension.First().ToString().ToUpper() + extension.Substring(1).ToLower();
+            Enum.TryParse(extension, out AudioType type);
+            return type;
+        }
+
+        public static int GenerateDuration(string path, AudioType type)
+        {
+            if (type == AudioType.Url)
+                return -1;
+
+            string durationString = getDurationStringFromShell(path);
+            return parseDurationStringToSeconds(durationString);
+        }
+
+
+
+
+
+        public static string getDurationStringFromShell(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            string directoryName = fileInfo.DirectoryName;
+            string filename = fileInfo.Name;
+
+            string fileLength = string.Empty;
+
+            var shell = new Shell();
+            var directory = shell.NameSpace(directoryName);
+            foreach (FolderItem2 file in directory.Items())
+            {
+                if (file.Name == filename)
+                    fileLength = directory.GetDetailsOf(file, 27).Trim();
+            }
+            Marshal.ReleaseComObject(directory);
+            Marshal.ReleaseComObject(shell);
+
+            return fileLength;
+        }
+
+        private static int parseDurationStringToSeconds(string durationString)
+        {
+            try
+            {
+                int hours = Int32.Parse(durationString.Substring(0, 2));
+                int minutes = Int32.Parse(durationString.Substring(3, 2));
+                int seconds = Int32.Parse(durationString.Substring(6, 2));
+                TimeSpan tp = new TimeSpan(hours, minutes, seconds);
+                return (int)tp.TotalSeconds;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
